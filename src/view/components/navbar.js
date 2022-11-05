68d5df1ec4235cfa507bac4b680078c847a1eb7f
korebhaumik <bhaumik.kore31@gmail.com>
2022-11-06 00:47:15 +0530

    korebhaumik/trello-clone

--- src/view/components/navbar.js
+++ src/view/components/navbar.js
@@ -166,6 +166,39 @@ export default function Navbar() {
         </div>
       </div>
       <hr />
+      <div className="mobile-search-engine">
+        {/* magnifying glass */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="mobile-icon-mg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+        <input placeholder="Search Task" />
+        {/* mic */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="mobile-icon-mic"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+          />
+        </svg>
+      </div>
     </>
   );
 }
